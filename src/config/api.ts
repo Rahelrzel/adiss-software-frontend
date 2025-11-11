2ed0054dbf5d4f7e0e425d79bcb43b55c7e09013
Rahelrzel <rahelrzele@gmail.com>
2025-11-11 09:47:41 +0300

    apiurl

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
-// export const apiUrl = import.meta.env.VITE_API_BASE_URL;
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = import.meta.env.VITE_API_BASE_URL;
+// export const apiUrl = "http://localhost:3000";
 export const authHeader = (token: string) => ({
   headers: { authorization: `Bearer ${token}` },
 });
